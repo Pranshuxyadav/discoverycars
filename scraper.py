9b659e7b21085c3ed00451c70e7d7e4867f7084e
Pranshu Yadav <107531073+Pranshuxyadav@users.noreply.github.com>
2024-07-13 05:55:02 +0530

    Update scraper.py
    cars24 mumbai

--- scraper.py
+++ scraper.py
@@ -12,8 +12,8 @@ import requests
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def find_cars_cars24():
-    url = 'https://www.cars24.com/buy-used-car?f=bodyType%3Ain%3Asuv&f=year%3Abw%3A2021%2C2021&sort=bestmatch&serveWarrantyCount=true&gaId=1057448734.1719685701&listingSource=TabFilter&storeCityId=2'
+def find_cars_cars24mumbai():
+    url = 'https://www.cars24.com/buy-used-cars-mumbai/'
     
     options = webdriver.ChromeOptions()
     options.add_argument('start-maximized')
@@ -80,6 +80,80 @@ def find_cars_cars24():
     
     finally:
         driver.quit()
+
+
+def find_cars_cars24delhi():
+    url = 'https://www.cars24.com/buy-used-car/?sort=bestmatch&serveWarrantyCount=true&gaId=1476729337.1720017156&storeCityId=2'
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('start-maximized')
+    options.add_argument('--headless')
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    service = Service("C:\\chromedriver-win64\\chromedriver.exe")
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+
+    try:
+        logging.info('Waiting for the page to load...')
+        wait = WebDriverWait(driver, 20)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'RPKrE')))
+        time.sleep(15)
+
+        logging.info('Scrolling the page...')
+        body = driver.find_element(By.TAG_NAME, 'body')
+        for _ in range(3):
+            body.send_keys(Keys.END)
+            time.sleep(10)
+
+        logging.info('Parsing the page source...')
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        cars_info = soup.find_all('a', class_='IIJDn')
+        cars_list = []
+
+        logging.info('Extracting car details...')
+        for car_info in cars_info:
+            car_title = car_info.find('h3', class_='_11dVb').text.strip() if car_info.find('h3', class_='_11dVb') else 'No title available'
+            car_details = ' '.join([li.text.strip() for li in car_info.find('ul', class_='_3J2G-').find_all('li')]) if car_info.find('ul', class_='_3J2G-') else 'No details available'
+            car_price = car_info.find('div', class_='_2KyOK').text.strip() if car_info.find('div', class_='_2KyOK') else 'No price available'
+            more_info = car_info['href'] if 'href' in car_info.attrs else 'No details available'
+            
+            car_dict = {
+                'title': car_title,
+                'details': car_details,
+                'price': car_price,
+                'link': more_info,
+                'image_url': 'No image available'
+            }
+            cars_list.append(car_dict)
+
+        logging.info('Extracting images...')
+        car_pictures = soup.find_all('div', class_='RPKrE')
+        for car_picture in car_pictures:
+            parent_anchor = car_picture.find_parent('a', class_='IIJDn')
+            if parent_anchor:
+                car_image_tag = car_picture.find('img')
+                car_image_url = car_image_tag['src'] if car_image_tag else 'No image available'
+                for car in cars_list:
+                    if car['link'] == parent_anchor['href']:
+                        car['image_url'] = car_image_url
+                        break
+
+        logging.info('Updating data.json with new data...')
+        update_json_file(cars_list)
+
+    except Exception as e:
+        logging.error(f'An error occurred: {e}')
+    
+    finally:
+        driver.quit() 
+
+
+
+
 
 def find_cars_droom():
     url = 'https://droom.in/cars'
@@ -153,5 +227,6 @@ def update_json_file(new_data):
             json.dump({'Sheet1': new_data}, f, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':
-    find_cars_cars24()
+    find_cars_cars24mumbai()
+    find_cars_cars24delhi()
     find_cars_droom()
